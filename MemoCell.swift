3b9ca62ea9de9fd905697f711406cca4fc484cf8
hyeri00 <nexusring@gmail.com>
2021-06-24 23:27:27 +0900

    feat(...) : 메모 작성 하기, 글 보여 주기

---
+++ MemoCell.swift
@@ -0,0 +1,23 @@
+//
+//  MemoCell.swift
+//  MyMemo
+//
+//  Created by 혜리 on 2021/06/24.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MemoData.swift
@@ -0,0 +1,18 @@
+//
+//  MemoData.swift
+//  MyMemo
+//
+//  Created by 혜리 on 2021/06/24.
+//
+
+import Foundation
+
+import UIKit
+
+class MemoData {
+    var memoIdx : Int? //데이터 식별 값
+    var title : String? //메모 제목
+    var contents : String? //메모 내용
+    var image : UIImage? //이미지
+    var regdate : Date? //작성일
+}
